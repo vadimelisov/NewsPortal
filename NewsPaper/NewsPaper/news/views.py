f63aaad15b97b9dc8d6d8d20666361320c508618
vadimelisov <126413765+vadimelisov@users.noreply.github.com>
2023-12-17 15:15:39 +0300

    Add files via upload

---
+++ NewsPaper/NewsPaper/news/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'news_detail.html'
+    context_object_name = 'post'
