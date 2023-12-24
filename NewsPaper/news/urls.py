94ac82dd651d0ccab5ca8fe54c1d10fca034a00a
vadimelisov <126413765+vadimelisov@users.noreply.github.com>
2023-12-24 14:40:19 +0300

    Add files via upload

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path
-
-from .views import NewsList, NewsDetail
+from .views import NewsList, NewsDetail, NewsSearch
 
 urlpatterns = [
     path('', NewsList.as_view(), name='post_list'),
+    path('search/', NewsSearch.as.view()),
     path('<int:pk>/', NewsDetail.as_view(), name='post_detail'),
 ]
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-
 from .models import Post
-
+from .filters import PostFilter
 
 
 class NewsList(ListView):
@@ -16,3 +15,20 @@ class NewsDetail(DetailView):
     model = Post
     template_name = 'news_detail.html'
     context_object_name = 'post'
+
+
+class NewsSearch(ListView):
+    model = Post
+    ordering = '-creation_date'
+    template_name = 'search.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
