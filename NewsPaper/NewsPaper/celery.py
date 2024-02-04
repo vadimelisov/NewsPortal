eaef44afba3551983dd8ad7e609cc4e3e33b76d9
vadimelisov <126413765+vadimelisov@users.noreply.github.com>
2024-02-04 21:32:37 +0300

    Add files via upload

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
@@ -7,3 +8,11 @@ app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_monday_8am': {
+        'task': 'news.tasks.weekly_send_email_task',
+        'schedule': crontab(),
+        'args': ()
+    }
+}
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -1,8 +1,62 @@
 from celery import shared_task
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from NewsPaper import settings
+from news.models import Post, Category
+import datetime
 
-import time
 
 @shared_task
-def hello():
-    time.sleep(10)
-    print("Hello, world!")
+def send_email_task(pk):
+    post = Post.objects.get(pk=pk)
+    categories = post.post_category.all()
+    title = post.post_title
+    subscribers_emails = []
+    for category in categories:
+        subscribers_users = category.subscribers.all()
+        for sub_user in subscribers_users:
+            subscribers_emails.append(sub_user.email)
+    html_content = render_to_string(
+        'post_created_email.html',
+        {
+            'text': f'{post.post_title}',
+            'link': f'{settings.SITE_URL}/news/{pk}',
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers_emails,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+
+@shared_task
+def weekly_send_email_task():
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(date_creation__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Category.objects.filter(category_name__in=categories).values_list('subscribers__email', flat=True))
+
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
